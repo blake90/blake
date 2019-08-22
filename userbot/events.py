6f48d6dfa30aa36e4a055ee2d92c1ad4319fd46b
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2019-08-22 19:45:29 +0530

    adding errors handler from base
    coz master phixed borked AFK :rofl:

--- userbot/events.py
+++ userbot/events.py
@@ -1,12 +1,12 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.b (the "License");
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
-""" Userbot module for managing events. One of the main components of the userbot. """
+""" Userbot module for managing events.
+ One of the main components of the userbot. """
 
 from telethon import events
-from telethon.events import StopPropagation
 import asyncio
 from userbot import bot
 from traceback import format_exc
@@ -37,3 +37,67 @@ def register(**args):
         return func
 
     return decorator
+
+
+def errors_handler(func):
+    async def wrapper(errors):
+        try:
+            await func(errors)
+        except BaseException:
+
+            date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+            new = {
+                'error': str(sys.exc_info()[1]),
+                'date': datetime.datetime.now()
+            }
+
+            text = "**Sorry, I encountered a error!**\n"
+            link = "[https://t.me/PaperplaneExtendedChat](Paperplane Extended Support)"
+            text += "If you wanna you can report it"
+            text += f"- just forward this message to {link}.\n"
+            text += "I won't log anything except the fact of error and date\n"
+
+            ftext = "\nDisclaimer:\nThis file uploaded ONLY here, "
+            ftext += "we logged only fact of error and date, "
+            ftext += "we respect your privacy, "
+            ftext += "you may not report this error if you've "
+            ftext += "any confidential data here, noone will see your data\n\n"
+            ftext += "--------BEGIN USERBOT TRACEBACK LOG--------"
+            ftext += "\nDate: " + date
+            ftext += "\nGroup ID: " + str(errors.chat_id)
+            ftext += "\nSender ID: " + str(errors.sender_id)
+            ftext += "\n\nEvent Trigger:\n"
+            ftext += str(errors.text)
+            ftext += "\n\nTraceback info:\n"
+            ftext += str(traceback.format_exc())
+            ftext += "\n\nError text:\n"
+            ftext += str(sys.exc_info()[1])
+            ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
+
+            command = "git log --pretty=format:\"%an: %s\" -5"
+
+            ftext += "\n\n\nLast 5 commits:\n"
+
+            process = await asyncio.create_subprocess_shell(
+                command,
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE
+            )
+            stdout, stderr = await process.communicate()
+            result = str(stdout.decode().strip()) \
+                + str(stderr.decode().strip())
+
+            ftext += result
+
+            file = open("error.log", "w+")
+            file.write(ftext)
+            file.close()
+
+            await errors.client.send_file(
+                errors.chat_id,
+                "error.log",
+                caption=text,
+            )
+            return
+
+    return wrapper
